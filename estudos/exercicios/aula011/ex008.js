9e91a7b77daf74dc8946ac999196059044bf38b1
rnathannunes <126528012+rnathannunes@users.noreply.github.com>
2023-05-21 20:31:41 -0300

    att

---
+++ estudos/exercicios/aula011/ex008.js
@@ -0,0 +1,8 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}else {
+    console.log(`Continue dirigindo com segurança.`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`) 
