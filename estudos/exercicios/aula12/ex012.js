e6a6b4e1116d2c6e8d98eae7f59b34ad8c5c9892
rnathannunes <126528012+rnathannunes@users.noreply.github.com>
2023-05-22 12:30:59 -0300

    att

--- estudos/exercicios/aula12/ex012.js
+++ estudos/exercicios/aula12/ex012.js
@@ -4,5 +4,7 @@ console.log(`Agora são exatamente ${hora} horas`)
 if (hora <= 12) {
     console.log('Bom dia!')
 } else if (hora <= 18) {
-    console.log('Boa')
+    console.log('Boa Tarde!')
+} else {
+    console.log(`Boa Noite!`)
 }
