ac8b283ac1d1972cdb1b9aff20952e16222909f5
rnathannunes <126528012+rnathannunes@users.noreply.github.com>
2023-05-30 17:06:51 -0300

    Aprendendo repetições em JS

---
+++ estudos/exercicios/aula13/ambiente.js
@@ -0,0 +1,15 @@
+ //Aprendendo repetições
+ 
+ var c = 1
+ while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
+ }
+
+ /*
+ var c = 1
+ do {
+    console.log(`Passo ${c}`)
+    c++
+ } while (c <= 6)
+*/
